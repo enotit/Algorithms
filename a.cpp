c9b5f4bcb7edff7697a10e4f75c3f0d41c72558a
enotit2 <43219870+enotit@users.noreply.github.com>
2023-04-24 17:17:19 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,102 +1,117 @@
 #include <iostream>
-#include <time.h>
-#include <string>
-#include <sstream>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <numeric>
+#include <functional>
 
-using namespace std;
-
-template<class T>
-class TNode {
-public:
-    T data;
-    TNode<T>* next;
-
-    TNode(T d) : data(d), next(nullptr) {}
-};
-
-template<class T>
-class CircularLinkedList {
-private:
-    TNode<T>* tail;
-
-public:
-    CircularLinkedList() : tail(nullptr) {}
-
-    void insert(T data) {
-        TNode<T>* node = new TNode<T>(data);
-        if (tail == nullptr) {
-            tail = node;
-            tail->next = tail;
-        } else {
-            node->next = tail->next;
-            tail->next = node;
-            tail = node;
-        }
-    }
-
-    void remove() {
-        if (tail == nullptr) {
-            cout << "Список пуст." << endl;
-            return;
-        }
-        TNode<T>* head = tail->next;
-        if (head == tail) {
-            delete head;
-            tail = nullptr;
-        } else {
-            tail->next = head->next;
-            delete head;
-        }
-    }
-
-    void clear(){
-        while (tail != nullptr) {
-            remove();
-        }
-    }
-    void print() {
-        if (tail == nullptr) {
-            cout << "Список пуст." << endl;
-            return;
-        }
-        TNode<T>* current = tail->next;
-        while (current != tail) {
-            cout << current->data << " ";
-            current = current->next;
-        }
-        cout << current->data << endl;
-    }
-    std::string getAddresses() {
-        std::stringstream ss;
-        if (tail != nullptr) {
-            TNode<T>* current = tail->next;
-            do {
-                ss << &(current->data) << " ";
-                current = current->next;
-            } while (current != tail->next);
-        }
-        return ss.str();
-    }
-};
-double take_now()
+struct MyStruct
 {
-    clock_t start = clock();
-    double seconds = (double)(start);
-    return seconds;
-}
+    int id;
+    std::string name;
 
-int main() {
-    CircularLinkedList<int> list;
-    for (int i = 1; i <= 5; i++) {
-        list.insert(i);
+    bool operator<(const MyStruct &other) const
+    {
+        return id < other.id;
     }
-    std::cout << "Адреса элементов после добавления: " << list.getAddresses() << std::endl; // 0x1c9ef3716d0 0x1c9ef371710 0x1c9ef371750 0x1c9ef371790 0x1c9ef3717d0 
+    bool operator>(const MyStruct &other) const
+    {
+        return id > other.id;
+    }
 
-    for (int i = 1; i <= 2; i++) {
-        list.remove();
+    bool operator==(const MyStruct &other) const
+    {
+        return id == other.id && name == other.name;
     }
-    std::cout << "Адреса элементов после удаления: " << list.getAddresses() << std::endl; // 0x1c9ef371750 0x1c9ef371790 0x1c9ef3717d0
+
+    friend std::ostream &operator<<(std::ostream &os, const MyStruct &s)
+    {
+        os << "(" << s.id << ", " << s.name << ")";
+        return os;
+    }
+};
+
+int main()
+{
+    // Задание 1: создание и заполнение контейнера
+    std::deque<MyStruct> myDeque = {
+        {5, "Женя"},
+        {3, "Жанара"},
+        {7, "Таисия"},
+        {2, "Анна"},
+        {9, "Михаил"}};
+
+    // Задание 2: замена максимального элемента минимальным
+    auto minIt = std::min_element(myDeque.begin(), myDeque.end());
+    auto maxIt = std::max_element(myDeque.begin(), myDeque.end());
+
+    if (minIt != myDeque.end() && maxIt != myDeque.end())
+    {
+        std::swap(*minIt, *maxIt);
+    }
+
+    std::cout << "После замены мин на макс: ";
+    for (const auto &elem : myDeque)
+    {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+
+    // Задание 3: удаление элементов меньше среднего
+    double sum = std::accumulate(myDeque.begin(), myDeque.end(), 0.0,
+                                 [](double acc, const MyStruct &s)
+                                 { return acc + s.id; });
+    double average = sum / myDeque.size();
+
+    auto removeIt = std::remove_if(myDeque.begin(), myDeque.end(),
+                                   [&](const MyStruct &s)
+                                   { return s.id < average; });
+
+    myDeque.erase(removeIt, myDeque.end());
+
+    std::cout << "После удаления числа меньше: ";
+    for (const auto &elem : myDeque)
+    {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+
+    // Задание 4: сортировка по убыванию
+    std::sort(myDeque.begin(), myDeque.end(), std::greater<MyStruct>());
+
+    std::cout << "После сортировки в порядке убывания: ";
+    for (const auto &elem : myDeque)
+    {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+
+    // Задание 5: нахождение медианы
+    if (!myDeque.empty())
+    {
+        std::size_t medianIndex = myDeque.size() / 2;
+        std::nth_element(myDeque.begin(), myDeque.begin() + medianIndex, myDeque.end());
+        std::cout << "Median: " << myDeque[medianIndex] << std::endl;
+    }
+
+    // Задание 6: Изменить знак каждого элемента в контейнере.
+    std::deque<int> d = {10, -20, 30, -40, 50};
+
+    std::cout << "Исходный контейнер: ";
+    for (auto i : d)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    std::transform(d.begin(), d.end(), d.begin(), std::negate<int>());
+
+    std::cout << "Контейнер после изменения знака: ";
+    for (auto i : d)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
-
 }

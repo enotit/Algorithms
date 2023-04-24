8881cc664d88e3b84fecac0cad89a2c7982d606b
enotit2 <43219870+enotit@users.noreply.github.com>
2023-04-24 15:44:43 +0300

    new task

--- a.cpp
+++ a.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <time.h>
+
 using namespace std;
 
 template<class T>
@@ -45,6 +47,11 @@ public:
         }
     }
 
+    void clear(){
+        while (tail != nullptr) {
+            remove();
+        }
+    }
     void print() {
         if (tail == nullptr) {
             cout << "Список пуст." << endl;
@@ -58,6 +65,12 @@ public:
         cout << current->data << endl;
     }
 };
+double take_now()
+{
+    clock_t start = clock();
+    double seconds = (double)(start);
+    return seconds;
+}
 
 int main() {
     CircularLinkedList<int> list;
@@ -74,6 +87,26 @@ int main() {
 
     list.remove();
     list.print();
+    list.clear();
+    long counts[5] = {100000, 200000, 300000, 400000, 500000};
+    for (long i = 0; i < 5; i++)
+    {
+        long count = counts[i];
+        double start = take_now();
+        for (int k = 0; k < count; k++)
+        {
+            list.insert(rand());
+        }
+        double finish = take_now();
+        double diff = (finish - start);
+        cout << "Вставка: " << count << " " << diff << endl;
+
+        start = take_now();
+        list.clear();
+        finish = take_now();
+        diff = (finish - start);
+        cout << "Удаление: " << count << " " << diff << endl;
+    }
 
     return 0;
 }
